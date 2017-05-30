abae5708553804873a0d497fe0c60c282cf753fb
Val75l <valentin.rigaud1@gmail.com>
2017-05-30 10:54:45 +0200

    Fin de creation de formulaire AngularJS

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,21 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
-}
+  $scope.users = [];
+  $scope.addUser = function () {
+
+    var firstName = $scope.userFirstName.trim();
+    $scope.userFirstName = '';
+    var lastName = $scope.userLastName.trim();
+    $scope.userLastName = '';
+    var mail = $scope.userMail.trim();
+    $scope.userMail = '';
+
+    if (firstName && lastName && mail) {
+      $scope.users.push({
+        firstname: firstName,
+        lastname: lastName,
+        email: mail
+      })
+    }
+  }
+};
